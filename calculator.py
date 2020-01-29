b9ecc92a63b4e35d4d86a5315688e978fa710058
Your Name <you@example.com>
2020-01-29 11:42:10 -0800

    Add function

--- calculator.py
+++ calculator.py
@@ -5,5 +5,13 @@ calculator program yourself in this file.
 """
 
 from arithmetic import
-token = input.split(" , ")
+number = 0
+while number = 0:
+    token = input()
+    token = token.split(" , ")
+    if token[0] == "q"
+    return
+    elif:
+        token[0] == "+"
+        add(token[1], token[2])
 # Your code goes here
