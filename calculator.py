03170243a2f5c8b9adb73e8297e087cb7ca5ac2d
Your Name <you@example.com>
2020-01-29 12:38:36 -0800

    Calculator-2

--- calculator.py
+++ calculator.py
@@ -4,14 +4,32 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import
-number = 0
-while number = 0:
-    token = input()
-    token = token.split(" , ")
-    if token[0] == "q"
-    return
-    elif:
-        token[0] == "+"
-        add(token[1], token[2])
-# Your code goes here
+from arithmetic import *
+def num():
+    while True:
+        token = input()
+        token = token.split(" ")
+        if token[0] == "q":
+            break
+        elif token[0] == "+":
+            print(add(int(token[1]), int(token[2])))
+        elif token[0] == '-':
+            print(subtract(int(token[1]), int(token[2])))
+        elif token[0] == "*":
+            print(multiply(int(token[1]), int(token[2])))
+        elif token[0] == "/":
+            print(divide(int(token[1]), int(token[2])))
+        elif token[0] == "square":
+            print(square(int(token[1])))
+        elif token[0] == "cube":
+            print(cube(int(token[1])))
+        elif token[0] == "pow":
+            print(power(int(token[1]), int(token[2])))
+        elif token[0] == "mod":
+            print(mod(int(token[1]), int(token[2])))
+        else:    
+            break
+
+
+num()
+
