e08a20fbad98c04b9d0965db9ee1db854def6aae
Your Name <you@example.com>
2020-01-29 11:35:56 -0800

    Quit Function

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import
+token = input.split(" , ")
+# Your code goes here
